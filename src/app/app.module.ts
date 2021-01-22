fe076c8bc9108f6008154c43f9d5e5de2d5a3b36
tkdalic <himagami126@gmail.com>
2021-01-22 20:36:12 +0900

    build: npx ng generate component parts/check-box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckBoxComponent } from './parts/check-box/check-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckBoxComponent
   ],
   imports: [
     BrowserModule,
