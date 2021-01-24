b417bfcf640df09d4e9d362c44fd79860c43fe06
tkdalic <himagami126@gmail.com>
2021-01-24 21:00:41 +0900

    modify: app routingの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PopulationCompositionComponent } from './pages/population-composition/population-composition.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PopulationCompositionComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
