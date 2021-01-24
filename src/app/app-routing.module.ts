1fb8fdcfd39d734ec7207e1ae4ea23b87c620485
tkdalic <himagami126@gmail.com>
2021-01-25 08:34:20 +0900

    add: 500のルーティングの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InternalServerErrorComponent } from './pages/internal-server-error/internal-server-error.component';
 import { PopulationCompositionComponent } from './pages/population-composition/population-composition.component';
 
 const routes: Routes = [
   { path: '', component: PopulationCompositionComponent },
+  { path: '500', component: InternalServerErrorComponent },
 ];
 
 @NgModule({
