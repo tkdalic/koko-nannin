9e036909d4934aed9c0d60499e7c22510ca7898b
tkdalic <himagami126@gmail.com>
2021-01-24 22:16:03 +0900

    modify: population composition 都道府県リストの表示

--- src/app/pages/population-composition/population-composition.component.ts
+++ src/app/pages/population-composition/population-composition.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from 'src/app/service/rest-api.service';
+import { PrefectureModel } from './interfaces/prefecture-model';
 
 @Component({
   selector: 'app-population-composition',
   templateUrl: './population-composition.component.html',
-  styleUrls: ['./population-composition.component.scss']
+  styleUrls: ['./population-composition.component.scss'],
 })
 export class PopulationCompositionComponent implements OnInit {
-
-  constructor() { }
+  prefectureInfoList: PrefectureModel[] = [];
+  constructor(private restApi: RestApiService) {}
 
   ngOnInit(): void {
+    this.restApi.getPrefectures().then(
+      (res) =>
+        (this.prefectureInfoList = res.result.map((item) => ({
+          ...item,
+          value: false,
+        })))
+    );
   }
-
 }
