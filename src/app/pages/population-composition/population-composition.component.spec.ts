c5f387988ea634ec55cf2ec51b3299c4e3e2dd07
tkdalic <himagami126@gmail.com>
2021-01-25 00:02:53 +0900

    modify: population composition configureTestingModuleの修正

--- src/app/pages/population-composition/population-composition.component.spec.ts
+++ src/app/pages/population-composition/population-composition.component.spec.ts
@@ -1,4 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ChartsModule } from 'ng2-charts';
+import { ChartComponent } from 'src/app/parts/chart/chart.component';
+import { CheckboxListItemComponent } from 'src/app/parts/checkbox-list-item/checkbox-list-item.component';
+import { CheckboxComponent } from 'src/app/parts/checkbox/checkbox.component';
+import { HeaderComponent } from 'src/app/parts/header/header.component';
+import { PageLayoutComponent } from 'src/app/templates/page-layout/page-layout.component';
 
 import { PopulationCompositionComponent } from './population-composition.component';
 
@@ -8,9 +19,23 @@ describe('PopulationCompositionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PopulationCompositionComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        CheckboxComponent,
+        CheckboxListItemComponent,
+        ChartComponent,
+        HeaderComponent,
+        PageLayoutComponent,
+        PopulationCompositionComponent,
+      ],
+      imports: [
+        FormsModule,
+        MatCheckboxModule,
+        MatCardModule,
+        ChartsModule,
+        HttpClientModule,
+        MatToolbarModule,
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
