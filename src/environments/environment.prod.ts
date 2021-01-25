70246686f12f75f8e32b3e4717be0e6f4c46d308
tkdalic <himagami126@gmail.com>
2021-01-25 09:28:01 +0900

    modify: environment tokenの追加

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  resas_token: process.env.RESAS_TOKEN,
+  resas_token: 'giZoMB9jeBtsCjWgQUW70tVkHsMG7HWkZLO89WD6',
   host_url: 'https://opendata.resas-portal.go.jp/api',
   host_version: 'v1',
 };
